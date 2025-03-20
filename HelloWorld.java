f1dacfe8377b910dc226cf3c70a00923df763059
JMaioLima <jmlimafonseca@gmail.com>
2025-03-20 15:13:41 +0000

    Add problem child

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from problem child");
+        System.out.println("Hello from conflict3");
     }
 }
