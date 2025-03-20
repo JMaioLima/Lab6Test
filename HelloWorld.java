7cb45bde32e79b0763bf098dfcb4f8c2ca696d0a
JMaioLima <jmlimafonseca@gmail.com>
2025-03-20 15:15:15 +0000

    Add problem parent

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from conflict3");
+        System.out.println("Hello from main");
     }
 }
